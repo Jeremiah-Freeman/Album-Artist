9015d08dcc4c687a8409d7978e7fc4f58e5527aa
Jay Freeman and Jake Campa <jake@>
2017-02-09 15:51:33 -0800

    add artist search functionality

--- app/app.php
+++ app/app.php
@@ -24,10 +24,44 @@
     });
 
     $app->post("/create_CD" , function() use ($app) {
-      $display_new_CD = new CD($_POST["title"] , null);
+      $display_new_CD = new CD($_POST["title"] , $_POST["artist"]);
       $display_new_CD->save();
       return $app["twig"]->render("display_created_CDs.html.twig" , array("display_CD" => $display_new_CD));
     });
+
+    $app->post("/artist_search" , function() use ($app) {
+      $name_of_artist = $_POST["artist-search"];
+      $artist_explode = explode(" ",$name_of_artist);
+
+
+      $matchedArray = array();
+
+      foreach ($_SESSION['list_of_CDs'] as $display_new_CD) {
+        for ($i = 0; $i < sizeof($artist_explode); $i++) {
+          if (strpos(strtoupper($display_new_CD->getArtist()), strtoupper($artist_explode[$i])) === false) {
+            # code...
+          } else {
+            if (strlen($artist_explode[$i]) === 1) {
+              # code.
+            } else {
+              array_push($matchedArray, $display_new_CD);
+              break;
+              # code...
+            }
+
+          }
+
+
+
+        }
+
+
+
+
+      }
+      return $app["twig"]->render("artist-search.html.twig" , array("artist" => $matchedArray));
+    });
+
 
 
 
